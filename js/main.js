f9ae88663153a5ce6800502fe596052a3db74feb
Micah Sterling <msterling89@gmail.com>
2022-01-11 14:55:12 -0600

    add file inport

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 import TableCsv from "./TableCsv.js";
 
 const tableRoot = document.querySelector("#csvRoot");
+const csvFileInput = document.querySelector("#csvFileInput");
 const tableCsv = new TableCsv(tableRoot);
 
-tableCsv.update([
-  [4500,"don",34],
-  [550,"mike",40],
-  [4500,"don",34],
-],["id","name","age"]);
 
+csvFileInput.addEventListener("change", () => {
+  Papa.parse(csvFileInput.files[0], {
+    delimiter: ",",
+    skipEmptyLines: true,
+    complete: (results) => {
+      tableCsv.update(results.data);
+    }
+  });
+});
